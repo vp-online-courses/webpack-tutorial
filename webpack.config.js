1b15ad59270e93dd3ffb20ede578779d9132b8aa
vp.online.courses <vp.online.courses@gmail.com>
2018-07-28 11:38:11 +0200

    Browser Caching

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
         publicPath: 'dist/'
     },
@@ -45,7 +45,7 @@ module.exports = {
     plugins: [
         new UglifyJsPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'styles.css'
+            filename: 'styles.[contenthash].css'
         })
     ]
 };
