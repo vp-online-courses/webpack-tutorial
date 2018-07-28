27a864eaeb56ae0ee4115a62d4c2c90a2e1222ad
vp.online.courses <vp.online.courses@gmail.com>
2018-07-28 10:56:11 +0200

    Handling SASS

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,17 +1,19 @@
-import './hello-world-button.css';
+import './hello-world-button.scss';
 
 class HelloWorldButton {
+    buttonCssClass = 'hello-world-button';
+
     render() {
         const button = document.createElement('button');
-        button.innerHTML = 'Hello world';
-        button.classList.add('hello-world-button');
         const body = document.querySelector('body');
+        button.innerHTML = 'Hello world';
         button.onclick = function () {
             const p = document.createElement('p');
             p.innerHTML = 'Hello world';
             p.classList.add('hello-world-text');
             body.appendChild(p);
         }
+        button.classList.add('hello-world-button');
         body.appendChild(button);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,12 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
             }
         ]
     }
