06e117906d79db436e857d29aabd3ba5eedae8e7
vp.online.courses <vp.online.courses@gmail.com>
2018-07-28 14:35:27 +0200

    How To Setup Development Environment for Multiple Page Application

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,9 +3,12 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'hello-world': './src/hello-world.js',
+        'kiwi': './src/kiwi.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, './dist'),
         publicPath: ''
     },
@@ -56,9 +59,18 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin('dist'),
         new HtmlWebpackPlugin({
+            filename: 'hello-world.html',
+            chunks: ['hello-world'],
             title: 'Hello world',
-            template: 'src/index.hbs',
-            description: 'some description'
+            description: 'Hello world',
+            template: 'src/page-template.hbs'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'kiwi.html',
+            chunks: ['kiwi'],
+            title: 'Kiwi',
+            description: 'Kiwi',
+            template: 'src/page-template.hbs'
         })
     ]
 };
