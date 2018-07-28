86690a205eb68dfe289cd1c43a6ccd479f539af3
vp.online.courses <vp.online.courses@gmail.com>
2018-07-28 14:03:05 +0200

    How to Create Multiple JavaScript and CSS Bundles

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,9 +4,12 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'hello-world': './src/hello-world.js',
+        'kiwi': './src/kiwi.js',
+    },
     output: {
-        filename: 'bundle.[contenthash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, './dist'),
         publicPath: ''
     },
@@ -51,7 +54,7 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'styles.[contenthash].css'
+            filename: '[name].[contenthash].css'
         }),
         new CleanWebpackPlugin('dist'),
         new HtmlWebpackPlugin({
