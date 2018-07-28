0676942f02ac6dfa1e3fd1d1478d68b4dfe2c850
vp.online.courses <vp.online.courses@gmail.com>
2018-07-28 12:55:11 +0200

    Mode

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ const heading = new Heading();
 heading.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Production mode');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('Development mode');
+}
+
+helloWorldButton.methodThatNotExists();
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         publicPath: ''
     },
-    mode: 'none',
+    mode: 'development',
     module: {
         rules: [
             {
