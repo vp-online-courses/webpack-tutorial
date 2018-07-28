d0bfff1a19a80349041f0d7c9361dbc74fd206dd
vp.online.courses <vp.online.courses@gmail.com>
2018-07-28 11:12:26 +0200

    Minification of the Resulting JavaScript Bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -39,5 +40,8 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new UglifyJsPlugin()
+    ]
 };
