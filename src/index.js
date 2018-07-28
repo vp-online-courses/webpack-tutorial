77eb06257c9e84d680e04378caed2b16109f4d56
vp.online.courses <vp.online.courses@gmail.com>
2018-07-28 13:46:51 +0200

    Remove heading2 and change button color back to green

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import Heading from './components/heading/heading.js';
 
 const heading = new Heading();
 heading.render();
-const heading2 = new Heading();
-heading2.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
