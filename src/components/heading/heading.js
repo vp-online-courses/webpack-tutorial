ccfda050741a4b7f527b438b0dbaabc1b28a9204
vp.online.courses <vp.online.courses@gmail.com>
2018-08-04 12:48:29 +0200

    Starting Point For Extra Lessons

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,10 +1,10 @@
 import './heading.scss';
 
 class Heading {
-    render() {
+    render(pageName) {
         const h1 = document.createElement('h1');
         const body = document.querySelector('body');
-        h1.innerHTML = 'Webpack is awesome';
+        h1.innerHTML = 'Webpack is awesome. This is "' + pageName + '" page';
         body.appendChild(h1);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 import Heading from './components/heading/heading.js';
+import _ from 'lodash';
 
 const heading = new Heading();
-heading.render();
+heading.render('hello world');
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
@@ -11,3 +12,5 @@ if (process.env.NODE_ENV === 'production') {
 } else if (process.env.NODE_ENV === 'development') {
     console.log('Development mode');
 }
+
+console.log(_.upperFirst('index module'));
