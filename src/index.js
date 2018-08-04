92df301959c3816ac655c4c6217a78cba5933db3
vp.online.courses <vp.online.courses@gmail.com>
2018-08-04 12:57:43 +0200

    Using Bootstrap With Webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 import Heading from './components/heading/heading.js';
 import _ from 'lodash';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 const heading = new Heading();
 heading.render('hello world');
