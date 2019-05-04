2427ff4d511acbf7d842d04e062ad91186581bc6
Viktor Pyskunov <vpyskunov@gmail.com>
2019-05-04 12:33:13 +0700

    8.6-integration-with-jquery

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,11 +1,12 @@
 import './heading.scss';
+import $ from 'jquery';
 
 class Heading {
     render(pageName) {
-        const h1 = document.createElement('h1');
-        const body = document.querySelector('body');
-        h1.innerHTML = 'Webpack is awesome. This is "' + pageName + '" page';
-        body.appendChild(h1);
+        const h1 = $('<h1>');
+        const body = $('body');
+        h1.text('Webpack is awesome. This is "' + pageName + '" page');
+        body.append(h1);
     }
 }
 
