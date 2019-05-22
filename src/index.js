b11ec0a33c6793827535ad4971ce0b01f96df10d
Viktor Pyskunov <vpyskunov@gmail.com>
2019-05-22 15:57:34 +0800

    Integration with FontAwesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 import Heading from './components/heading/heading.js';
 import _ from 'lodash';
+
+library.add(faSpinner);
+dom.watch();
 
 const heading = new Heading();
 heading.render('hello world');
