fd6d3722c7f79ddf1e207c2997c5264504510ef3
Viktor Pyskunov <vpyskunov@gmail.com>
2020-12-30 20:22:53 +0200

    serving-html-pages-via-express-js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
 
 app.get('/', function (req, res) {
-    res.send('Some dummy content');
+    const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, function () {
