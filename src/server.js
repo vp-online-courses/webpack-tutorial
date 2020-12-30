6fa7e5f6cc01cc65bfc9c3558663b6bb0d9e68ac
Viktor Pyskunov <vpyskunov@gmail.com>
2020-12-30 20:50:40 +0200

    handling-static-files

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
+app.use('/static', express.static(path.resolve(__dirname, '../dist')));
+
 app.get('/', function (req, res) {
     const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
     const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
