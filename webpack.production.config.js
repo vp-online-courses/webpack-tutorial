3c2b78bf4c1cfc62445e97aea3539463ad47a4a1
Viktor Pyskunov <vpyskunov@gmail.com>
2021-01-06 19:47:37 +0200

    Add a new script "start", adjust public path to /static/

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath: ''
+        publicPath: '/static/'
     },
     mode: 'production',
     optimization: {
