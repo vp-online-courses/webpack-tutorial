519f21f483419f58c43f888cae909aaee399a006
Viktor Pyskunov <vpyskunov@gmail.com>
2021-03-05 11:06:30 +0300

    Split css chunks

--- src/about.js
+++ src/about.js
@@ -1,7 +1,7 @@
 import Heading from './components/heading/heading.js';
 import _ from 'lodash';
 import 'bootstrap';
-import './index.scss';
+import './about.scss';
 
 const heading = new Heading();
 heading.render('hello world');
