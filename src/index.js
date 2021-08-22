756a5df457b5fd2c3255091abcf525e65dec2c21
Viktor Pyskunov <vpyskunov@gmail.com>
2021-08-22 12:42:10 +0300

    Configure webpack-dev-server v4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import Heading from './components/heading/heading.js';
 
 const heading = new Heading();
 heading.render();
+const heading2 = new Heading();
+heading2.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -10,6 +10,16 @@ module.exports = {
         publicPath: ''
     },
     mode: 'development',
+    devServer: {
+        port: 9000,
+        static: {
+            directory: path.resolve(__dirname, './dist'),
+        },
+        devMiddleware: {
+            index: 'index.html',
+            writeToDisk: true
+        }
+    },
     module: {
         rules: [
             {
