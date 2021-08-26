41bfc495b8840e9e47b8a11fe42a6e363c0f7768
Viktor Pyskunov <vpyskunov@gmail.com>
2021-08-26 20:52:49 +0300

    Update webpack-dev-server configuration

--- kiwi/webpack.dev.config.js
+++ kiwi/webpack.dev.config.js
@@ -11,9 +11,14 @@ module.exports = {
     },
     mode: 'development',
     devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
-        index: 'kiwi.html',
-        port: 9002
+        port: 9000,
+        static: {
+            directory: path.resolve(__dirname, './dist'),
+        },
+        devMiddleware: {
+            index: 'index.html',
+            writeToDisk: true
+        }
     },
     module: {
         rules: [
