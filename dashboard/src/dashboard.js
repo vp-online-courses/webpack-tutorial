17af78d58ebcbb5e16a27685de9b2dc6a23a1f92
Viktor Pyskunov <vpyskunov@gmail.com>
2023-01-24 15:54:43 +0200

    Client-side routing in dashboard Microfrontend

--- dashboard/src/dashboard.js
+++ dashboard/src/dashboard.js
@@ -2,34 +2,45 @@ import NavigationBar from './components/navigation-bar/navigation-bar.js';
 
 const navigationItems = [
     {
-        url: '/hello-world-page',
+        url: '#hello-world-page',
         title: 'Hello World Page'
     },
     {
-        url: '/kiwi-page',
+        url: '#kiwi-page',
         title: 'Kiwi Page'
     }
 ];
 const navigationBar = new NavigationBar();
 navigationBar.render(navigationItems);
 
-
-const url = window.location.pathname;
-if (url === '/hello-world-page') {
-    import('HelloWorldApp/HelloWorldPage')
-        .then(HelloWorldPageModule => {
-            const HelloWorldPage = HelloWorldPageModule.default;
-            const helloWorldPage = new HelloWorldPage();
-            helloWorldPage.render();
-        });
-} else if (url === '/kiwi-page') {
-    import('KiwiApp/KiwiPage')
-        .then(KiwiPageModule => {
-            const KiwiPage = KiwiPageModule.default;
-            const kiwiPage = new KiwiPage();
-            kiwiPage.render();
-        });
+function removePreviousElements() {
+    const elementsToRemove = Array.from(document.body.children).slice(1);
+    elementsToRemove.forEach(element => {
+        element.parentNode.removeChild(element);
+    });
 }
+
+window.addEventListener('hashchange', function (event) {
+    const url = window.location.hash;
+    console.log(url, event)
+    if (url === '#hello-world-page') {
+        import('HelloWorldApp/HelloWorldPage')
+            .then(HelloWorldPageModule => {
+                removePreviousElements();
+                const HelloWorldPage = HelloWorldPageModule.default;
+                const helloWorldPage = new HelloWorldPage();
+                helloWorldPage.render();
+            });
+    } else if (url === '#kiwi-page') {
+        import('KiwiApp/KiwiPage')
+            .then(KiwiPageModule => {
+                removePreviousElements();
+                const KiwiPage = KiwiPageModule.default;
+                const kiwiPage = new KiwiPage();
+                kiwiPage.render();
+            });
+    }
+});
 
 
 console.log('dashboard');
