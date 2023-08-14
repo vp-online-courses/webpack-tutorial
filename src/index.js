8ef1bc243a621a6e7a006db3b6e67358ebd0b45e
Bahram Movlanov <bmovlanov@gmail.com>
2023-08-13 19:23:29 -0600

    set up

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@
 
 // There is a separate video explaining how to use Github repository in this course. 
 // In this video I talk about how to switch between branches and how to use the repository.
+
+helloWorld();
